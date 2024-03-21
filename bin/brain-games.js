3ae60834b406588322c7dcdf4ab2a59a12a43a83
Irina Bugrova <irina92-08@mail.ru>
2024-03-21 18:57:53 +0300

    feat: add eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { name } from '../src/cli.js';
+
 console.log('node bin/brain-games.js');
-console.log ('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!');
 name();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
-export const name = () => {
-const result = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${result}!`);
-};
 
+export const name = () => {
+  const result = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${result}!`);
+};
