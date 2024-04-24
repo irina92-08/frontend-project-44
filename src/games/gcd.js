06ad2abffbb5ea899bf312a44f4d709a23851370
Irina Bugrova <irina92-08@mail.ru>
2024-04-24 08:06:27 +0300

    modified:   src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,26 +1,25 @@
 import { runEngine } from '../index.js';
 import { getRandomInRange } from '../utils.js';
 
-const resultGcd = (num1, num2) => {
-  const minNum = num1 < num2 ? num1 : num2;
-  const maxNum = !minNum;
-  if (maxNum % minNum === 0) {
-    return minNum;
-  }
-  const halfMinNum = Math.round(minNum / 2);
-  for (let i = halfMinNum; i > 0; i -= 1) {
-    if (minNum % i === 0 && maxNum % i === 0) {
-      return i;
+const getDivizor = (x, y) => {
+  let num1 = x;
+  let num2 = y;
+
+  while(num1 > 0 && num2 > 0) {
+    if(num1 >= num2) {
+      num1 %= num2;
+    } else {
+      num2 %= num1;
     }
   }
-  return num1;
+  return Math.max(num1, num2);
 };
 
 const generateRoundGcd = () => {
-  const oneNum = getRandomInRange(1, 100);
-  const twoNum = getRandomInRange(1, 100);
+  const oneNum = getRandomInRange();
+  const twoNum = getRandomInRange();
 
-  const answerGcd = resultGcd(oneNum, twoNum);
+  const answerGcd = getDivizor(oneNum, twoNum);
   const questionGcd = (`${oneNum} ${twoNum}`);
 
   return [`${String(questionGcd)}`, `${String(answerGcd)}`];
