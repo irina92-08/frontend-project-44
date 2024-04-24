21ea02a48d5f6687657521012f2a135f26a8cd2a
Irina Bugrova <irina92-08@mail.ru>
2024-04-24 08:12:25 +0300

    lint

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,8 +5,8 @@ const getDivizor = (x, y) => {
   let num1 = x;
   let num2 = y;
 
-  while(num1 > 0 && num2 > 0) {
-    if(num1 >= num2) {
+  while (num1 > 0 && num2 > 0) {
+    if (num1 >= num2) {
       num1 %= num2;
     } else {
       num2 %= num1;
